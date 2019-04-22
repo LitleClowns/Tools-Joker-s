db05a478cfa839d61f04e7530281f26b64cd790c
Litle Clowns <49826385+LitleClowns@users.noreply.github.com>
2019-04-22 21:27:09 +0700

    Add files via upload

---
+++ project.sh
@@ -0,0 +1,288 @@
+clear
+clear
+# Variables
+b='\033[1m'
+u='\033[4m'
+bl='\E[30m'
+r='\E[31m'
+g='\E[32m'
+bu='\E[34m'
+m='\E[35m'
+c='\E[36m'
+w='\E[37m'
+endc='\E[0m'
+enda='\033[0m'
+blue='\e[1;34m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+
+
+figlet -f standard "Welcome" | lolcat
+sleep 3
+clear
+echo "************** °°° **************" | lolcat
+sleep 1
+echo "Cread by : J0K3R•S" | lolcat
+echo "Contact  : cyberteam62@gmail.com" | lolcat
+echo "Youtube  : Creater Clowns" | lolcat
+sleep 1
+echo "************** °°° **************"
+
+clear
+
+
+###################################################
+# CTRL C
+###################################################
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo  "(Ctrl + C ) Detected, Trying To Exit ... "|lolcat
+sleep 1
+echo ""
+echo "Thank for Coly ing ... "| lolcat
+sleep 1
+echo ""
+echo "#Coly in here  ... "| lolcat
+exit
+}
+
+
+
+figlet -f digital.flf "TOOLS-INSTALLERv1" | lolcat
+
+
+
+date | lolcat
+
+
+
+echo ""
+echo "1. Install Admin-Finder${enda}" | lolcat
+echo "-------------- ••• --------------" | lolcat
+echo "2. Install RED_HAWK${enda}" | lolcat
+echo "-------------- ••• --------------" |lolcat
+echo "3. Install MBF${enda}" | lolcat
+echo "-------------- ••• --------------" | lolcat
+echo "4. Install Weeman${enda}" | lolcat
+echo "-------------- ••• --------------" |lolcat
+echo "5. Install OSIF${enda}" | lolcat
+echo "-------------- ••• --------------" |lolcat
+echo "6. Install Fb-Brute${enda}" | lolcat
+echo "-------------- ••• --------------" |lolcat
+echo "7. Install Tools-TuanBadut${enda}" | lolcat
+echo "-------------- ••• --------------" |lolcat
+echo "8. Install Tools-CIA${enda}" | lolcat
+echo "-------------- ••• --------------" |lolcat
+echo "9. Install Tools-CIA${enda}" | lolcat
+echo "-------------- ••• --------------" |lolcat
+echo "10. Install Instahack${enda}" | lolcat
+echo "-------------- ••• --------------" |lolcat
+echo "11. Install Tools-SpamWA${enda}" | lolcat
+echo "-------------- ••• --------------" |lolcat
+echo "12. Install autoreportFB${enda}" | lolcat
+echo "-------------- ••• --------------" |lolcat
+echo "13. Install Temux-Style${enda}" | lolcat
+echo "-------------- ••• --------------" |lolcat
+echo "14. Install sqlmap${enda}" | lolcat
+echo "-------------- ••• --------------" |lolcat
+echo "15. Install Tool-x${enda}" | lolcat
+echo "-------------- ••• --------------" |lolcat
+echo "16. Install Virus-Creator${enda}" | lolcat
+echo "-------------- ••• --------------" |lolcat
+echo "17. Install Tools-BAJINGANv6${enda}" | lolcat
+echo "-------------- ••• --------------" |lolcat
+echo "18. Install Tools-LITETOOLS${enda}" | lolcat
+echo "-------------- ••• --------------" |lolcat
+echo "19. Install Tools-BugHunter${enda}" | lolcat
+echo "-------------- ••• --------------" |lolcat
+echo "20. Install Tools-Lazymux${enda}" | lolcat
+echo "-------------- ••• --------------" |lolcat
+echo "21. Install Tools-CIAv1${enda}" | lolcat
+echo "-------------- ••• --------------" |lolcat
+
+echo "99. Exit   " | lolcat
+echo ""
+
+read -p "root@clown:~# " cia ;
+
+case $cia in
+
+1) echo "Waiting for progress..." 
+sleep 1
+cd $HOME 
+git clone https://github.com/the-c0d3r/admin-finder.git
+sleep 2
+echo "Instalizing Complate..." | lolcat
+;;
+
+2) echo "Waiting for progress..." 
+sleep 1
+cd $HOME 
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+sleep 2
+echo "Instalizing Complate..." | lolcat
+;;
+
+3) echo "Waiting for progress..." 
+sleep 1
+cd $HOME 
+git clone https://github.com/YukersCreew/mbf
+sleep 2
+echo "Instalizing Complate..." | lolcat
+;;
+
+4) echo "Waiting for progress..." 
+sleep 1
+cd $HOME 
+git clone https://github.com/evait-security/weeman
+sleep 2
+echo "Instalizing Complate..." | lolcat
+;;
+
+5) echo "Waiting for progress..." 
+sleep 1
+cd $HOME 
+git clone https://github.com/CiKu370/OSIF
+sleep 2
+echo "Instalizing Complate..." | lolcat
+;;
+
+6) echo "Waiting for progress..." 
+sleep 1
+cd $HOME
+git clone https://github.com/IqbalzNoobs/fb-brute
+sleep 2
+echo "Instalizing Complate..." | lolcat
+;;
+
+7) echo "Waiting for progress..." 
+sleep 1
+cd $HOME
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv3
+sleep 2
+echo "Instalizing Complate..." | lolcat
+;;
+
+8) echo "Waiting for progress..." 
+sleep 1
+cd $HOME
+git clone https://github.com/Killerclown12/cia_gank
+sleep 2
+echo "Instalizing Complate..." | lolcat
+;;
+
+9) echo "Waiting for progress..." 
+sleep 1
+cd $HOME
+git clone https://github.com/4L13199/LITESPAM
+sleep 2
+echo "Instalizing Complate..." | lolcat
+;;
+
+10) echo "Waiting for progress..." 
+sleep 1
+cd $HOME
+git clone https://github.com/Pure-L0G1C/Instagram
+sleep 2
+echo "Instalizing Complate..." | lolcat
+;;
+
+11) echo "Waiting for progress..." 
+sleep 1
+cd $HOME
+git clone https://github.com/shadowbot82/Spam-wa
+sleep 2
+echo "Instalizing Complate..." | lolcat
+;;
+
+12) echo "Waiting for progress..." 
+sleep 1
+cd $HOME
+git clone https://github.com/SIIL3NT/auto-report
+sleep 2
+echo "Instalizing Complate..." | lolcat
+;;
+
+13) echo "Waiting for progress..." 
+sleep 1
+cd $HOME
+git clone https://github.com/IqbalzNoobs/termux_style
+sleep 2
+echo "Instalizing Complate..." | lolcat
+;;
+
+14) echo "Waiting for progress..." 
+sleep 1
+cd $HOME
+git clone https://github.com/sqlmapproject/sqlmap
+sleep 2
+echo "Instalizing Complate..." | lolcat
+;;
+
+15) echo "Waiting for progress..." 
+sleep 1
+cd $HOME
+git clone https://github.com/Rajkumrdusad/Tool-X 
+sleep 2
+echo "Instalizing Complate..." | lolcat
+;;
+
+
+16) echo "Waiting for progress..." 
+sleep 1
+cd $HOME
+git clone https://github.com/DarknessCyberTeam/BAJINGANv6
+sleep 2
+echo "Instalizing Complate..." | lolcat
+;;
+
+17) echo "Waiting for progress..." 
+sleep 1
+cd $HOME
+git clone https://github.com/DarkCurut08/Virus
+sleep 2
+echo "Instalizing Complate..." | lolcat
+;;
+
+18) echo "Waiting for progress..." 
+sleep 1
+cd $HOME
+git clone https://github.com/4L13199/LITETOOLS
+sleep 2
+echo "Instalizing Complate..." | lolcat
+;;
+
+19) echo "Waiting for progress..." 
+sleep 1
+cd $HOME
+git clone https://github.com/thehackingsage/bughunter
+sleep 2
+echo "Instalizing Complate..." | lolcat
+;;
+
+20) echo "Waiting for progress..." 
+sleep 1
+cd $HOME
+git clone https://github.com/Gameye98/Lazymux
+sleep 2
+echo "Instalizing Complate..." | lolcat
+;;
+
+21) echo "Waiting for progress..." 
+sleep 1
+cd $HOME
+git clone https://github.com/Killerclown12/TOOL-CIA
+sleep 2
+echo "Instalizing Complate..." | lolcat
+;;
+
+99) echo "Terima Kasih Telah Memakai Tool Kami" | lolcat
+echo "Silakan Kembali Lagi " |lolcat
+echo "See You Again Ferguso" |lolcat
+exit
+;;
+*) echo "Soory muka anda terlalu buriq"
+esac
+done
+done
